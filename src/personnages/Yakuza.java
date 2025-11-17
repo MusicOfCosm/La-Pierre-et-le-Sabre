3837d6ad0a4c48f9cce3bd53e1bef989a5f9e048
Matthieu Courtines <matthieu.courtines@gmail.com>
2025-11-17 19:11:59 +0100

    Les Yakuza et leur clan

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -13,13 +13,19 @@ public class Yakuza extends Humain {
 		return this.reputation;
 	}
 	
+	public void direBonjour() {
+		super.direBonjour();
+		this.parler("Mon clan est celui de " + this.clan + ".");
+	}
+	
+	
 	public void extorquer(Commercant victime) {
 		super.parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
 		super.parler(victime.getNom() + ", si tu tiens à la vie donne moi ta bourse !");
 		int argentVole = victime.seFaireExtorquer();
 		super.gagnerArgent(argentVole);
 		this.reputation += 1;
-		super.parler("J'ai piqu� les " + argentVole + " sous de " + victime.getNom() +
+		super.parler("J'ai piqué les " + argentVole + " sous de " + victime.getNom() +
 					 ", ce qui me fait " + super.getArgent() + " sous dans ma poche. Hi ! Hi !");
 	}
 	
